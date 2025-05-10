aaeb04dc82d35b38c86cd5a6713910766b7f529f
Fahad <fahad.khan2216@gmail.com>
2025-05-10 17:07:17 +0530

    extention folder removed

--- apps/extention/src/injectApp.ts
+++
@@ -1,7 +0,0 @@
-import { mountBountyButton, mountBountyPopup } from './App';
-import './index.css'
-import '@solana/wallet-adapter-react-ui/styles.css';
-console.log("injectApp.ts loaded");
-
-mountBountyButton();
-mountBountyPopup();
